1bf2cbd2a9a0aba753882ed2a9517a04dba562ee
benwasden <benjaminpaulwasden@gmail.com>
2026-02-12 08:08:25 -0800

    Initial Commit

---
+++ Study Cards/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Study Cards
+//
+//  Created by Benjamin Wasden on 2/12/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Study Cards/Study_CardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Study_CardsApp.swift
+//  Study Cards
+//
+//  Created by Benjamin Wasden on 2/12/26.
+//
+
+import SwiftUI
+
+@main
+struct Study_CardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
